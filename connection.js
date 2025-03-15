3f51978a538c80d1c53d14fd1aa2502532f333d8
vsn20 <vuppalasainaman@gmail.com>
2025-03-15 14:38:36 +0530

    deleet all files

--- connection.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-async function connectmongodb(url){
-    return mongoose.connect(url);
-}
-
-module.exports ={
-    connectmongodb,
-};
--- routes/staticrouter.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-
-const router = express.Router();
-
-router.get("/",(req,res)=>res.render("home.ejs"));
-router.get("/login",(req,res)=>res.render("login.ejs"))
-
-module.exports= router;
