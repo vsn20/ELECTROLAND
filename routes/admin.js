4aa43a1e68a5e5a65f38c345fc2d167c108c015d
vsn20 <vuppalasainaman@gmail.com>
2025-03-15 14:39:41 +0530

    delete all

--- routes/admin.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-
-const router = express.Router();
-
-router.get("/addsalesmanager",(req,res)=>res.render("owner/addsalesmanager.ejs"));
-
-module.exports=router;
