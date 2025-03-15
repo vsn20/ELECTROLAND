d50b0a3c19d46e3fb27ecf45ad81c1ac1761d2f1
vsn20 <vuppalasainaman@gmail.com>
2025-03-15 23:04:05 +0530

    changed

--- routes/admin.js
+++ routes/admin.js
@@ -33,10 +33,9 @@ router.post("/branches/edit/:bid", update_branch);
 //admin-customers
 const { customers_display} = require("../controllers/admin_customers_display");
 router.get("/customers", customers_display);
-module.exports=router;
 
 //admin-inventory
-
-//inventory
 const {inventory_display}=require("../controllers/inventory_display");
 router.get("/stocks",inventory_display);
+
+module.exports=router;
