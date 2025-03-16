9d97688f4f2cd1c8329878e344f07e9da1ac4a95
vsn20 <vuppalasainaman@gmail.com>
2025-03-16 22:27:08 +0530

    DELETED

--- routes/login.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const router = express.Router();
-
-const {handlelogin}= require("../controllers/login");
-
-router.post("/",handlelogin);
-
-module.exports=router;
