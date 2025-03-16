393981fdcc49bc33c12e78c269da914e695a526c
vsn20 <vuppalasainaman@gmail.com>
2025-03-16 12:20:24 +0530

    home

--- routes/salesmanager.js
+++ routes/salesmanager.js
@@ -2,6 +2,11 @@ const express = require("express");
 
 const router = express.Router();
 
+//salesmanager-home
+router.get("/home",(req,res)=>res.render("salesmanager/home",{
+    activePage: 'employee',
+    activeRoute: '',
+}))
 //salesmanager-inventory
 const {inventory_display}=require("../controllers/salesmanager/inventory_display");
 router.get("/stocks",inventory_display);
