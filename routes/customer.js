e566746801da945455c6d042721b27e780347234
vsn20 <vuppalasainaman@gmail.com>
2025-03-18 00:00:14 +0530

    changed everything

--- routes/customer.js
+++ routes/customer.js
@@ -1,6 +1,25 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/home", (req, res) => res.render("customer/home"));
+router.get("/home", (req, res) =>res.redirect("/customer/previouspurchases"));
 
+
+//previous purchases
+const {previous_data_display,total_previous_data_display}=require("../controllers/customer/previous_data");
+router.get("/previouspurchases",previous_data_display);
+router.get("/previouspurchases/totaldetails/:saleid",total_previous_data_display);
+
+//complaints
+const {complaint_data,complaint_datadetails}=require("../controllers/customer/complaints");
+router.get("/complaints",complaint_data);
+router.get("/complaints/totaldetails/:saleid",complaint_datadetails);
+
+//reviews
+const{ review_display,review_datadetails,}=require("../controllers/customer/reviews");
+router.get("/review",review_display);
+router.get("/review/totaldetails/:saleid",review_datadetails);
+
+//blogs
+const{blogs}=require("../controllers/customer/blogs")
+router.get("/blogs",blogs);
 module.exports = router;
