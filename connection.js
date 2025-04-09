bbdb83afc4b5b045b299c13033b4c900ea9bd12d
vsn20 <vuppalasainaman@gmail.com>
2025-04-09 22:44:13 +0530

    asdf

--- connection.js
+++ connection.js
@@ -1,21 +1,18 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-let isConnected = false; // Flag to track connection status
+async function connectToMongoDB() {
+  try {
+    await mongoose.connect('mongodb+srv://vuppalasainaman:LMcCkdsA06cVLl3o@cluster0.q0nzt0i.mongodb.net/electroworld', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 5000, // Fail faster if connection isn’t possible
+    });
+    console.log('Connected to MongoDB Atlas');
+  } catch (error) {
+    console.error('MongoDB connection failed:', error);
+    process.exit(1); // Exit if connection fails
+  }
+}
 
-const connectmongodb = async () => {
-    if (isConnected) {
-        console.log("MongoDB already connected");
-        return;
-    }
+module.exports = connectToMongoDB;
     
-    try {
-        await mongoose.connect("mongodb+srv://vuppalasainaman:LMcCkdsA06cVLl3o@cluster0.q0nzt0i.mongodb.net/electroworld");
-        isConnected = true;
-        console.log("MongoDB connected successfully");
-    } catch (err) {
-        console.error("MongoDB connection failed:", err);
-        process.exit(1);
-    }
-};
-
-module.exports = connectmongodb;
--- controllers/company_display.js
+++ controllers/company_display.js
@@ -1,4 +1,4 @@
-const Company = require("../models/Company");
+const Company = require("../models/company");
 
 async function company_display(req, res) {
     try {
