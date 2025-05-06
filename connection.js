9cfc4a8b08403e14950ab2c74f0a25802f1608ba
vsn20 <vuppalasainaman@gmail.com>
2025-05-07 01:33:00 +0530

    asdfg

--- connection.js
+++ connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function connectToMongoDB() {
   try {
-    await mongoose.connect('mongodb+srv://vuppalasainaman:LMcCkdsA06cVLl3o@cluster0.q0nzt0i.mongodb.net/electroworld', {
+    await mongoose.connect('mongodb+srv://ELECTROLAND:ELECTROLAND@cluster0.mc5xp0c.mongodb.net/ELECTROLAND', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       serverSelectionTimeoutMS: 5000, // Fail faster if connection isn’t possible
